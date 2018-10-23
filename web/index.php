9f9fa84c1a3f522076470de679929b2c03345e61
jc.g <kuro.jg@gmail.com>
2018-10-23 12:58:22 -0500

    df
    sdf

--- web/index.php
+++ web/index.php
@@ -21,5 +21,12 @@ $app->get('/', function() use($app) {
   $app['monolog']->addDebug('logging output.');
   return $app['twig']->render('index.twig');
 });
+echo "HOLA";
+$to = "jgarcia@intt2.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: somebodyelse@example.com";
 
+mail($to,$subject,$txt,$headers);
 $app->run();
